c4f42ee2c1637bcfeafc91b063e645e896d57301
PedroBarbosa239 <barbosajs239@gmail.com>
2023-02-07 23:11:01 -0400

    curriculum vitae_1_versão

---
+++ arquivo.js
@@ -0,0 +1,23 @@
+function botao(){
+
+
+    experiencia_div.style.height = "300px";
+    experiencia_div.style.transition = "ease-in-out 0.3s";
+    
+    texto_experiencia.style.display = "block";
+    
+    seta.style.display = "none";
+    botao_2.style.display = "block";
+}
+
+function botao2(){
+    
+    experiencia_div.style.height = "150px";
+    experiencia_div.style.transition = "ease-in-out 0.5s";
+
+    texto_experiencia.style.display = "none";
+
+    seta.style.display = "block";
+    botao_2.style.display = "none";
+}
+
